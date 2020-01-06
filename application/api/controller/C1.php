80bcfefb700690b5e9b7e5da059f3bdc6dbc2b88
1159981713 <1159981713@qq.com>
2020-01-06 12:02:24 +0800

    修改

--- application/api/controller/C1.php
+++ application/api/controller/C1.php
@@ -25,13 +25,30 @@ class C1 extends Base
     {
         
         $list = db('c1_win_level')
-            ->order('sort asc')
+            ->order('sort desc')
             ->select();
         foreach ($list as $k=>$v) {
             $list[$k]['count'] = db('c1_win')->where(['level_id'=>$v['id']])->count();
         }
+        $data = [];
+        $data['list'] = $list;
+        $all = db('c1_user')->select();
+        $count = 0;
+        $win_count = 0;
+        foreach ($all as $v) {
+            if ($v['status'] ==1) {
+                $win_count ++;
+            }
+            $count ++;
+        }
+        $data['count'] = $count;
+        $data['win_count'] = $win_count;
+        $setting = db('setting')->where('k="c1_title" OR k="c1_top_title"')->select();
+        foreach ($setting as $v) {
+            $data[$v['k']] = $v['value'];
+        }
 
-        $this->out(200,$list);
+        $this->out(200,$data);
         
     }
 
